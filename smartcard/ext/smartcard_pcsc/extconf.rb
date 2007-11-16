37b44100bd047cba31017ac34d9cc43498c1b079
Victor Costan <costan@gmail.com>
2007-11-16 21:37:34 +0000

    Better header file identification in extconf.rb for the PC/SC ruby extension.

--- smartcard/ext/smartcard_pcsc/extconf.rb
+++ smartcard/ext/smartcard_pcsc/extconf.rb
@@ -1,17 +1,25 @@
 require 'mkmf'
+require 'pp'
 
 $LDFLAGS ||= ''
 
 pcsc_headers = []
+['wintypes.h', 'reader.h', 'winscard.h'].each do |header|
+  ['', 'PCSC/', './pcsc_surrogate_'].each do |path_prefix|
+    if have_header(path_prefix + header)
+      pcsc_headers.push((path_prefix[0,1] == '.') ? "\"#{path_prefix + header}\"" : "<#{path_prefix + header}>")
+      break
+    end
+  end
+end
+
 pcsc_defines = []
 if RUBY_PLATFORM =~ /darwin/
-  pcsc_headers += ['<PCSC/winscard.h>']
-  pcsc_headers += ['"pcsc_surrogate_wintypes.h"', '"pcsc_surrogate_reader.h"']
   $LDFLAGS += ' -framework PCSC'
 elsif RUBY_PLATFORM =~ /win/
-  pcsc_headers += ['<winscard.h>']
+  # TODO: no clue what to do here
 else
-  pcsc_headers += ['<winscard.h>']
+  have_library('pcsclite')
 end
 
 File.open('pcsc_include.h', 'w') do |f|
