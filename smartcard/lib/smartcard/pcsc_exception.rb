09cfe7822b3f627674ef1e269cde1543b27f22df
Victor Costan <costan@gmail.com>
2008-05-01 14:51:10 +0000

    Documentation fixes. Updated version to 0.3.1.

--- smartcard/lib/smartcard/pcsc_exception.rb
+++ smartcard/lib/smartcard/pcsc_exception.rb
@@ -1,3 +1,5 @@
+# Contains information about an exception at the PC/SC layer.
 class Smartcard::PCSC::Exception  
+  # The error number returned by the failed PC/SC function call. Should be one of the Smartcard::PCSC::SCARD_E_ constants.
   attr_reader :errno
 end
